3c2f48c20b3a24a0bbfb1df8f95931710e7bb14b
Leigh Clancy <lclancyau@googlemail.com>
2020-08-19 16:45:51 +0100

    attempting to fix netlify build

--- functions/get-posts.js
+++ functions/get-posts.js
@@ -1,8 +1,6 @@
 const fetch = require('node-fetch')
 
 exports.handler = async () => {
-  console.log(process.env.API_URL)
-
   const response = await fetch(`${process.env.API_URL}/articles`)
     .then(res => res.json())
     .catch(err => console.error(err)) // eslint-disable-line
