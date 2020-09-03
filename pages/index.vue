195009280beaeffa889676e2171fadc330efc2ee
Leigh Clancy <lclancyau@googlemail.com>
2020-09-03 10:39:21 +0100

    adding netlify cms

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,13 @@
 </template>
 
 <script>
-export default {}
+export default {
+  head () {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }]
+    }
+  }
+}
 </script>
 
 <style>
