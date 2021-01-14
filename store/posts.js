9c21f348a4ab297e899fa0664af39485f0b69cce
Leigh Clancy <lclancyau@googlemail.com>
2021-01-14 14:43:11 +1100

    adding headers to netlify function request

--- store/posts.js
+++ store/posts.js
@@ -8,9 +8,16 @@ export const mutations = {
   }
 }
 
+const HEADERS = {
+  'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin',
+  'Content-Type': 'application/json', // optional
+  'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  'Access-Control-Max-Age': '8640'
+}
+
 export const actions = {
   async getPosts ({ commit }) {
-    const thePosts = await fetch(`${process.env.NTL_URL}/.netlify/functions/get-posts`)
+    const thePosts = await fetch(`${process.env.NTL_URL}/.netlify/functions/get-posts`, HEADERS)
       .then(res => res.json())
       .catch(err => console.error(err)) // eslint-disable-line
 
