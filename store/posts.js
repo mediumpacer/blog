4370636bad1bd913bbbbd237fb5562a0579bb213
Leigh Clancy <lclancyau@googlemail.com>
2021-01-14 14:53:27 +1100

    reverting added header info

--- store/posts.js
+++ store/posts.js
@@ -8,16 +8,9 @@ export const mutations = {
   }
 }
 
-const HEADERS = {
-  'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin',
-  'Content-Type': 'application/json', // optional
-  'Access-Control-Allow-Methods': 'POST, OPTIONS',
-  'Access-Control-Max-Age': '8640'
-}
-
 export const actions = {
   async getPosts ({ commit }) {
-    const thePosts = await fetch(`${process.env.NTL_URL}/.netlify/functions/get-posts`, HEADERS)
+    const thePosts = await fetch(`${process.env.NTL_URL}/.netlify/functions/get-posts`)
       .then(res => res.json())
       .catch(err => console.error(err)) // eslint-disable-line
 
